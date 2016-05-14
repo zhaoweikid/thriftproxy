1ecdf2500153cb26c1832cf284a964b7b99c9412
zhaoweikid <zhaoweikid@163.com>
2016-05-15 00:15:43 +0800

    change

--- bin/endian_swap.h
+++ bin/endian_swap.h
@@ -7,9 +7,9 @@
 #define htob16(x) OSSwapHostToBigInt16(x)
 #define htob32(x) OSSwapHostToBigInt32(x)
 #define htob64(x) OSSwapHostToBigInt64(x)
-#define b16toh(x) OSSwapBigToHostInt16(x)
-#define b32toh(x) OSSwapBigToHostInt32(x)
-#define b64toh(x) OSSwapBigToHostInt64(x)
+#define btoh16(x) OSSwapBigToHostInt16(x)
+#define btoh32(x) OSSwapBigToHostInt32(x)
+#define btoh64(x) OSSwapBigToHostInt64(x)
 
 #else
 
@@ -28,16 +28,16 @@
 #define htob64(x) bswap_64(x)
 #endif
 
-#ifndef b16toh
-#define b16toh(x) bswap_16(x)
+#ifndef btoh16
+#define btoh16(x) bswap_16(x)
 #endif
 
-#ifndef b32toh
-#define b32toh(x) bswap_32(x)
+#ifndef btoh32
+#define btoh32(x) bswap_32(x)
 #endif
 
-#ifndef b64toh
-#define b64toh(x) bswap_64(x)
+#ifndef btoh64
+#define btoh64(x) bswap_64(x)
 #endif
 
 #endif
